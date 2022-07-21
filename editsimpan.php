813b2888e05b6ee5da4928a899e8f9f9216e8f6f
yorsbae <77182622+yorsbae@users.noreply.github.com>
2022-07-21 10:25:58 +0700

    Update editsimpan.php

--- editsimpan.php
+++ editsimpan.php
@@ -8,7 +8,7 @@ $nilai_tkw=$_POST["nilai_tkw"];
 $query="UPDATE uas SET nama='".$nama."',nilai_tkp='".$nilai_tkp."',nilai_tiu='".$nilai_tiu."',nilai_tkw='".$nilai_tkw."' WHERE no_pend='".$no_pend."'";
 
 if($con->query($query)==TRUE) {
-    header("location:input.php");
+    header("location:index.php");
 }else{
     echo "<script>alert('PESAN GAGAL DISIMPAN'):history.back();</script>";
 }
